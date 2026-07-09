4ad35e6b876f7700d074ad109d93a87a0d292552
Massooma <mukhtar.massooma@gmail.com>
2026-07-09 18:09:51 +0200

    refactor : improved book controller REST mappings

--- src/main/java/fr/masooma/bookapi/controller/BookController.java
+++ src/main/java/fr/masooma/bookapi/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/books")
 public class BookController {
 
     private final BookService bookService;
@@ -15,27 +16,27 @@ public class BookController {
         this.bookService = bookService;
     }
 
-    @GetMapping("/books")
+    @GetMapping
     public List<Book> getBooks(){
         return bookService.getAllBooks();
     }
 
-    @PostMapping("/books")
+    @PostMapping
     public Book createBook(@RequestBody Book book){
         return bookService.saveBook(book);
     }
 
-    @DeleteMapping("/books/{id}")
+    @DeleteMapping("/{id}")
     public void deleteBook(@PathVariable Long id){
         bookService.deleteBook(id);
     }
 
-    @PutMapping("books/{id}")
+    @PutMapping("/{id}")
     public Book updateBook(@PathVariable Long id, @RequestBody Book newBook){
         return bookService.updateBook(id,newBook);
     }
 
-    @GetMapping("books/{id}")
+    @GetMapping("/{id}")
     public Book getBookById(@PathVariable Long id){
         return bookService.getById(id);
     }
